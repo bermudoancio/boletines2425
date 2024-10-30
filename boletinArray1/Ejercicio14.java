e33400020b9dbfec713942818ed3463378ce9f31
Jose <bermudo@gmail.com>
2024-10-30 09:58:17 +0100

    Ejercicio 14 y mientradasalida

---
+++ boletinArray1/Ejercicio14.java
@@ -0,0 +1,20 @@
+package boletinArray1;
+
+import comun.MiEntradaSalida;
+
+public class Ejercicio14 {
+    private static int[] cola;
+    private static int numElementos;
+
+    public static void main(String[] args) {
+        int numero = MiEntradaSalida.solicitarEntero("Introduce un número");
+    }
+
+    public static void encolar(int n) {
+
+    }
+
+    public static int desencolar() {
+
+    }
+}
